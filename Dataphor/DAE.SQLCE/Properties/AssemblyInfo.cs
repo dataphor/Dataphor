ffc4d12adc718b0ae221737fbaa1ae0edf8eaecb
bryn <bryn@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2008-05-08 23:04:37 +0000

    Added SQLCE and SQLite assemblies.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@16 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/DAE.SQLCE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Alphora Dataphor - SQL CE Store Support")]
+[assembly: AssemblyDescription("Provides the implementation of a SQL CE store for use by the DAE.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alphora")]
+[assembly: AssemblyProduct("Dataphor")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("Dataphor")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4cad2af6-55e3-4e0d-8fa6-dab5b2f9832a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
---
+++ Dataphor/DAE.SQLite/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Alphora Dataphor - SQLite Support")]
+[assembly: AssemblyDescription("Provides the implementation of a SQLite store for use by the DAE.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alphora")]
+[assembly: AssemblyProduct("Dataphor")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("Dataphor")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38f13da3-d531-41c3-bc8e-2a898d9674c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
