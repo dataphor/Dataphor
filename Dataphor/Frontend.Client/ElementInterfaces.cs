6b4ccdbaa8fece34b762472b5cfd0cb2a5699721
rob <rob@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2008-10-04 00:05:39 +0000

    Removed HideDate and HideTime from the interface
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/branches/2.1@68 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/Frontend.Client/ElementInterfaces.cs
+++ Dataphor/Frontend.Client/ElementInterfaces.cs
@@ -82,8 +82,6 @@ namespace Alphora.Dataphor.Frontend.Client
 	/// in the Sample10 [dfd] document.</example>
 	public interface IDateTimeBox : IAlignedElement
 	{
-        bool HideDate { get; set; }
-        bool HideTime { get; set; }
 	}
 
 	/// <summary> Allows the triggering of an action (button). </summary> <doc/>
