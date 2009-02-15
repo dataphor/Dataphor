ae2a5a9859ffcd52a22ffcebbfd3b520b3cf7302
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-02-15 21:56:51 +0000

    BaseUserControl (for Darwen Mdi)
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@104 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/Dataphoria/BaseUserControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Alphora.Dataphor.Dataphoria
+{
+    public partial class BaseUserControl : UserControl, IStatusBarClient
+    {
+        public BaseUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
