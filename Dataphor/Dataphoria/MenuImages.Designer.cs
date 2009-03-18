22dd59740699d6bc375da4a09462769027ed30d6
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-03-18 12:39:45 +0000

    delete & rename image
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@139 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/Dataphoria/MenuImages.Designer.cs
+++ Dataphor/Dataphoria/MenuImages.Designer.cs
@@ -8,11 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Alphora.Dataphor.Dataphoria
-{
+namespace Alphora.Dataphor.Dataphoria {
     using System;
-
-
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -23,308 +22,256 @@ namespace Alphora.Dataphor.Dataphoria
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "2.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class MenuImages
-    {
-
+    internal class MenuImages {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal MenuImages()
-        {
+        internal MenuImages() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Alphora.Dataphor.Dataphoria.MenuImages", typeof(MenuImages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
-
-        internal static System.Drawing.Bitmap Close
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Close {
+            get {
                 object obj = ResourceManager.GetObject("Close", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Connect
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Connect {
+            get {
                 object obj = ResourceManager.GetObject("Connect", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Copy
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Copy {
+            get {
                 object obj = ResourceManager.GetObject("Copy", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Cut
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Cut {
+            get {
                 object obj = ResourceManager.GetObject("Cut", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Disconnect
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Delete {
+            get {
+                object obj = ResourceManager.GetObject("Delete", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Disconnect {
+            get {
                 object obj = ResourceManager.GetObject("Disconnect", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap ExpandOnDemand
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap ExpandOnDemand {
+            get {
                 object obj = ResourceManager.GetObject("ExpandOnDemand", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Find
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Find {
+            get {
                 object obj = ResourceManager.GetObject("Find", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap FindNext
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap FindNext {
+            get {
                 object obj = ResourceManager.GetObject("FindNext", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Help
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Help {
+            get {
                 object obj = ResourceManager.GetObject("Help", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap LaunchForm
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap LaunchForm {
+            get {
                 object obj = ResourceManager.GetObject("LaunchForm", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap New
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap New {
+            get {
                 object obj = ResourceManager.GetObject("New", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap NewScript
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap NewScript {
+            get {
                 object obj = ResourceManager.GetObject("NewScript", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Open
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Open {
+            get {
                 object obj = ResourceManager.GetObject("Open", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap OpenWith
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap OpenWith {
+            get {
                 object obj = ResourceManager.GetObject("OpenWith", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Paste
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Paste {
+            get {
                 object obj = ResourceManager.GetObject("Paste", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Print
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Print {
+            get {
                 object obj = ResourceManager.GetObject("Print", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Redo
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Redo {
+            get {
                 object obj = ResourceManager.GetObject("Redo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Registered
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Registered {
+            get {
                 object obj = ResourceManager.GetObject("Registered", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Replace
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Rename {
+            get {
+                object obj = ResourceManager.GetObject("Rename", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        internal static System.Drawing.Bitmap Replace {
+            get {
                 object obj = ResourceManager.GetObject("Replace", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Save
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Save {
+            get {
                 object obj = ResourceManager.GetObject("Save", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap SaveAll
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap SaveAll {
+            get {
                 object obj = ResourceManager.GetObject("SaveAll", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap SaveDocument
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap SaveDocument {
+            get {
                 object obj = ResourceManager.GetObject("SaveDocument", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap SaveFile
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap SaveFile {
+            get {
                 object obj = ResourceManager.GetObject("SaveFile", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap SelectAll
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap SelectAll {
+            get {
                 object obj = ResourceManager.GetObject("SelectAll", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap SplitWindow
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap SplitWindow {
+            get {
                 object obj = ResourceManager.GetObject("SplitWindow", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Undo
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Undo {
+            get {
                 object obj = ResourceManager.GetObject("Undo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Website
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Website {
+            get {
                 object obj = ResourceManager.GetObject("Website", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Zoom_In
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Zoom_In {
+            get {
                 object obj = ResourceManager.GetObject("Zoom_In", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
-        internal static System.Drawing.Bitmap Zoom_Out
-        {
-            get
-            {
+        
+        internal static System.Drawing.Bitmap Zoom_Out {
+            get {
                 object obj = ResourceManager.GetObject("Zoom_Out", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
