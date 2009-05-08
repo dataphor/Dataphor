9615d1cb0fcfa57551e979551f4c7371236ad77b
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-05-08 05:23:43 +0000

    General cleanup
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@183 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/Dataphoria/FormDesigner/CustomFormDesigner.cs
+++ Dataphor/Dataphoria/FormDesigner/CustomFormDesigner.cs
@@ -4,239 +4,240 @@
 	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
 */
 using System;
-using System.Drawing;
 using System.Collections;
-using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Windows.Forms;
-using System.Xml;
 using System.IO;
-
-using Alphora.Dataphor.DAE;
+using System.Xml;
 using Alphora.Dataphor.BOP;
-using Alphora.Dataphor.Frontend.Client;
-using Alphora.Dataphor.Dataphoria;
+using Alphora.Dataphor.DAE.Runtime.Data;
 using Alphora.Dataphor.Dataphoria.Designers;
+using Alphora.Dataphor.Frontend.Client;
+using Alphora.Dataphor.Frontend.Client.Windows;
+using Serializer=Alphora.Dataphor.Frontend.Client.Serializer;
 
 namespace Alphora.Dataphor.Dataphoria.FormDesigner
 {
-	public class CustomFormDesigner : FormDesigner
-	{
-		private System.ComponentModel.Container components = null;
+    public class CustomFormDesigner : FormDesigner
+    {
+        private Container FComponents;
+        private Ancestors FAncestors;
 
-		public CustomFormDesigner() : base()	// dummy constructor for SyncFusion's MDI menu merging
-		{
-			InitializeComponent();
-		}
+        public CustomFormDesigner() // dummy constructor for SyncFusion's MDI menu merging
+        {
+            InitializeComponent();
+        }
 
-		public CustomFormDesigner(IDataphoria ADataphoria, string ADesignerID) : base(ADataphoria, ADesignerID)
-		{
-			InitializeComponent();
-		}
+        public CustomFormDesigner(IDataphoria ADataphoria, string ADesignerID) : base(ADataphoria, ADesignerID)
+        {
+            InitializeComponent();
+        }
 
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if(components != null)
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
+        [Browsable(false)]
+        public Ancestors Ancestors
+        {
+            get { return FAncestors; }
+        }
 
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.Size = new System.Drawing.Size(300,300);
-			this.Text = "CustomFormDesigner";
-		}
-		#endregion
+        protected override void Dispose(bool ADisposing)
+        {
+            if (ADisposing)
+            {
+                if (FComponents != null)
+                {
+                    FComponents.Dispose();
+                }
+            }
+            base.Dispose(ADisposing);
+        }
 
-		private Ancestors FAncestors;
-		[Browsable(false)]
-		public Ancestors Ancestors
-		{
-			get { return FAncestors; }
-		}
+        public override void New()
+        {
+            var LAncestors = new Ancestors();
+            var LDocumentExpression = new DocumentExpression();
+            LDocumentExpression.Type = DocumentType.Document;
+            LDocumentExpression.DocumentArgs.OperatorName = ".Frontend.Form";
+            LAncestors.Add(DocumentExpressionForm.Execute(LDocumentExpression));
+            New(LAncestors);
+        }
 
-		public override void New()
-		{
-			Ancestors LAncestors = new Ancestors();
-			Frontend.Client.Windows.DocumentExpression LDocumentExpression = new Frontend.Client.Windows.DocumentExpression();
-			LDocumentExpression.Type = Frontend.Client.Windows.DocumentType.Document;
-			LDocumentExpression.DocumentArgs.OperatorName = ".Frontend.Form";
-			LAncestors.Add(DocumentExpressionForm.Execute(LDocumentExpression));
-			New(LAncestors);
-		}
+        /// <summary> Initializes the designer from a list of ancestor document expressions. </summary>
+        /// <param name="AAncestors"> List of ancestor document expressions. Must be non-empty. </param>
+        public void New(Ancestors AAncestors)
+        {
+            XmlDocument LAncestor = MergeAncestors(AAncestors);
+            InternalNew(HostFromDocumentData(LAncestor, String.Empty), true);
+            UpdateReadOnly(LAncestor);
+            FAncestors = AAncestors;
+        }
 
-		/// <summary> Initializes the designer from a list of ancestor document expressions. </summary>
-		/// <param name="AAncestors"> List of ancestor document expressions. Must be non-empty. </param>
-		public void New(Ancestors AAncestors)
-		{
-			XmlDocument LAncestor = MergeAncestors(AAncestors);
-			InternalNew(HostFromDocumentData(LAncestor, String.Empty), true);
-			UpdateReadOnly(LAncestor);
-			FAncestors = AAncestors;
-		}
+        public void CheckHostsDocument(IHost AHost)
+        {
+            if ((AHost.Document == null) || (AHost.Document == String.Empty))
+                throw new DataphoriaException(DataphoriaException.Codes.CannotCustomizeDocument);
+        }
 
-		public void CheckHostsDocument(IHost AHost)
-		{
-			if ((AHost.Document == null) || (AHost.Document == String.Empty))
-				throw new DataphoriaException(DataphoriaException.Codes.CannotCustomizeDocument);
-		}
+        public new void New(IHost AHost)
+        {
+            CheckHostsDocument(AHost);
+            InternalNew(AHost, false);
+            FAncestors = new Ancestors();
+            FAncestors.Add(AHost.Document);
+            UpdateReadOnly(MergeAncestors(FAncestors));
+        }
 
-		public new void New(IHost AHost)
-		{
-			CheckHostsDocument(AHost);
-			InternalNew(AHost, false);
-			FAncestors = new Ancestors();
-			FAncestors.Add(AHost.Document);
-			UpdateReadOnly(MergeAncestors(FAncestors));
-		}
+        public override void Open(IHost AHost)
+        {
+            CheckHostsDocument(AHost);
+            DocumentDesignBuffer LBuffer = BufferFromHost(AHost);
+            Service.ValidateBuffer(LBuffer);
+            var LDocument = new DilxDocument();
+            LDocument.Read(LBuffer.LoadData());
+            XmlDocument LMergedAncestors = MergeAncestors(LDocument.Ancestors); // do this first in case of errors
+            SetDesignHost(AHost, false);
+            Service.SetBuffer(LBuffer);
+            Service.SetModified(false);
+            FAncestors = LDocument.Ancestors;
+            UpdateReadOnly(LMergedAncestors);
+        }
 
-		public override void Open(IHost AHost)
-		{
-			CheckHostsDocument(AHost);
-			DocumentDesignBuffer LBuffer = BufferFromHost(AHost);
-			Service.ValidateBuffer(LBuffer);
-			DilxDocument LDocument = new DilxDocument();
-			LDocument.Read(LBuffer.LoadData());
-			XmlDocument LMergedAncestors = MergeAncestors(LDocument.Ancestors);	// do this first in case of errors
-			SetDesignHost(AHost, false);
-			Service.SetBuffer(LBuffer);
-			Service.SetModified(false);
-			FAncestors = LDocument.Ancestors;
-			UpdateReadOnly(LMergedAncestors);
-		}
+        protected override void RequestLoad(DesignService AService, DesignBuffer ABuffer)
+        {
+            var LDocument = new DilxDocument();
+            LDocument.Read(ABuffer.LoadData());
 
-		protected override void RequestLoad(DesignService AService, DesignBuffer ABuffer)
-		{
-			DilxDocument LDocument = new DilxDocument();
-			LDocument.Read(ABuffer.LoadData());
+            Ancestors LAncestors;
+            IHost LHost;
+            XmlDocument LAncestor = null;
+            if (LDocument.Ancestors.Count >= 1)
+            {
+                LAncestors = LDocument.Ancestors;
+                LAncestor = MergeAncestors(LAncestors);
+                var LMerge = new XmlDocument();
+                LMerge.LoadXml(LDocument.Content);
+                var LCurrent = (XmlDocument) LAncestor.CloneNode(true);
+                Inheritance.Merge(LCurrent, LMerge);
+                LHost = HostFromDocumentData(LCurrent, GetDocumentExpression(ABuffer));
+            }
+            else
+            {
+                LHost = HostFromDocumentData(LDocument.Content, GetDocumentExpression(ABuffer));
+                LAncestors = null;
+            }
+            SetDesignHost(LHost, true);
+            UpdateReadOnly(LAncestor);
+            FAncestors = LAncestors;
+        }
 
-			Ancestors LAncestors;
-			IHost LHost;
-			XmlDocument LAncestor = null;
-			if (LDocument.Ancestors.Count >= 1)
-			{
-				LAncestors = LDocument.Ancestors;
-				LAncestor = MergeAncestors(LAncestors);
-				XmlDocument LMerge = new XmlDocument();
-				LMerge.LoadXml(LDocument.Content);
-				XmlDocument LCurrent = (XmlDocument)LAncestor.CloneNode(true);
-				Inheritance.Merge(LCurrent, LMerge);
-				LHost = HostFromDocumentData(LCurrent, GetDocumentExpression(ABuffer));
-			}
-			else
-			{
-				LHost = HostFromDocumentData(LDocument.Content, GetDocumentExpression(ABuffer));
-				LAncestors = null;
-			}
-			SetDesignHost(LHost, true);
-			UpdateReadOnly(LAncestor);
-			FAncestors = LAncestors;
-		}
+        private XmlDocument MergeAncestors(Ancestors AAncestors)
+        {
+            XmlDocument LDocument = null;
+            // Process any ancestors
+            foreach (string LAncestor in AAncestors)
+            {
+                if (LDocument == null)
+                    LDocument = LoadDocument(LAncestor);
+                else
+                    Inheritance.Merge(LDocument, LoadDocument(LAncestor));
+            }
+            return LDocument;
+        }
 
-		private XmlDocument MergeAncestors(Ancestors AAncestors)
-		{
-			XmlDocument LDocument = null;
-			// Process any ancestors
-			foreach (string LAncestor in AAncestors)
-			{
-				if (LDocument == null)
-					LDocument = LoadDocument(LAncestor);
-				else
-					Inheritance.Merge(LDocument, LoadDocument(LAncestor));
-			}
-			return LDocument;
-		}
+        private XmlDocument LoadDocument(string ADocument)
+        {
+            var LResult = new XmlDocument();
+            using (DataValue LResultData = FrontendSession.Pipe.RequestDocument(ADocument))
+            {
+                try
+                {
+                    LResult.LoadXml(LResultData.AsString);
+                }
+                catch (Exception LException)
+                {
+                    throw new DataphoriaException(DataphoriaException.Codes.InvalidXMLDocument, LException,
+                                                  LResultData.ToString());
+                }
+            }
+            return LResult;
+        }
 
-		private XmlDocument LoadDocument(string ADocument)
-		{
-			XmlDocument LResult = new XmlDocument();
-			using (DAE.Runtime.Data.DataValue LResultData = FrontendSession.Pipe.RequestDocument(ADocument))
-			{
-				try
-				{
-					LResult.LoadXml(LResultData.AsString);
-				}
-				catch (Exception LException)
-				{
-					throw new DataphoriaException(DataphoriaException.Codes.InvalidXMLDocument, LException, LResultData.ToString());
-				}
-			}
-			return LResult;
-		}
+        private static string XmlDocumentToString(XmlDocument ADocument)
+        {
+            var LWriter = new StringWriter();
+            ADocument.Save(LWriter);
+            return LWriter.ToString();
+        }
 
-		private static string XmlDocumentToString(XmlDocument ADocument)
-		{
-			StringWriter LWriter = new StringWriter();
-			ADocument.Save(LWriter);
-			return LWriter.ToString();
-		}
+        protected override void RequestSave(DesignService AService, DesignBuffer ABuffer)
+        {
+            var LDocument = new DilxDocument();
+            if (FAncestors != null)
+                LDocument.Ancestors = FAncestors;
 
-		protected override void RequestSave(DesignService AService, DesignBuffer ABuffer)
-		{
-			DilxDocument LDocument = new DilxDocument();
-			if (FAncestors != null)
-				LDocument.Ancestors = FAncestors;
+            var LContent = new XmlDocument();
+            Serializer LSerializer = FrontendSession.CreateSerializer();
+            LSerializer.Serialize(LContent, DesignHost.Children[0]);
+            Dataphoria.Warnings.AppendErrors(this, LSerializer.Errors, true);
 
-			XmlDocument LContent = new XmlDocument();
-			Frontend.Client.Serializer LSerializer = FrontendSession.CreateSerializer();
-			LSerializer.Serialize(LContent, DesignHost.Children[0]);
-			Dataphoria.Warnings.AppendErrors(this, LSerializer.Errors, true);
-			
-			if (FAncestors != null)
-				LContent = Inheritance.Diff(MergeAncestors(LDocument.Ancestors), LContent);
-			StringWriter LWriter = new StringWriter();
-			XmlTextWriter LXmlWriter = new XmlTextWriter(LWriter);
-			LXmlWriter.Formatting = Formatting.Indented;
-			LXmlWriter.Indentation = 3;
-			LContent.WriteContentTo(LXmlWriter);
-			LDocument.Content = LWriter.ToString();	// only the document node
+            if (FAncestors != null)
+                LContent = Inheritance.Diff(MergeAncestors(LDocument.Ancestors), LContent);
+            var LWriter = new StringWriter();
+            var LXmlWriter = new XmlTextWriter(LWriter);
+            LXmlWriter.Formatting = Formatting.Indented;
+            LXmlWriter.Indentation = 3;
+            LContent.WriteContentTo(LXmlWriter);
+            LDocument.Content = LWriter.ToString(); // only the document node
 
-			ABuffer.SaveData(LDocument.Write());
-			UpdateHostsDocument(ABuffer);
-		}
+            ABuffer.SaveData(LDocument.Write());
+            UpdateHostsDocument(ABuffer);
+        }
 
-		private void UpdateReadOnly(XmlDocument AAncestor)
-		{
-			DesignerNode LRootNode = (DesignerNode)FNodesTree.Nodes[0];
-			if (AAncestor != null)
-			{
-				Hashtable LNodesByName = new Hashtable(StringComparer.OrdinalIgnoreCase);
-				BuildNodesByName(LNodesByName, LRootNode);
-				foreach (XmlNode LNode in AAncestor.DocumentElement.ChildNodes) // (skips root interface)
-					UpdateNodeReadOnly(LNode, LNodesByName);
-			}
-		}
+        private void UpdateReadOnly(XmlDocument AAncestor)
+        {
+            var LRootNode = (DesignerNode) FNodesTree.Nodes[0];
+            if (AAncestor != null)
+            {
+                var LNodesByName = new Hashtable(StringComparer.OrdinalIgnoreCase);
+                BuildNodesByName(LNodesByName, LRootNode);
+                if (AAncestor.DocumentElement != null)
+                    foreach (XmlNode LNode in AAncestor.DocumentElement.ChildNodes) // (skips root interface)
+                        UpdateNodeReadOnly(LNode, LNodesByName);
+            }
+        }
 
-		private static void BuildNodesByName(Hashtable ANodesByName, DesignerNode ANode)
-		{
-			if (ANode.Node.Name != String.Empty)
-				ANodesByName.Add(ANode.Node.Name, ANode);
-			foreach (DesignerNode LNode in ANode.Nodes)
-				BuildNodesByName(ANodesByName, LNode);
-		}
+        private static void BuildNodesByName(Hashtable ANodesByName, DesignerNode ANode)
+        {
+            if (ANode.Node.Name != String.Empty)
+                ANodesByName.Add(ANode.Node.Name, ANode);
+            foreach (DesignerNode LNode in ANode.Nodes)
+                BuildNodesByName(ANodesByName, LNode);
+        }
 
-		private static void UpdateNodeReadOnly(XmlNode ANode, Hashtable ANodesByName) 
-		{
-			XmlAttribute LName = ANode.Attributes[BOP.Persistence.CXmlBOPName];
-			if (LName != null) 
-			{
-				DesignerNode LDesignerNode = (DesignerNode)ANodesByName[LName.Value];
-				if (LDesignerNode != null)
-					LDesignerNode.SetReadOnly(true, false);
-			}
-			foreach (XmlNode LNode in ANode.ChildNodes)
-				UpdateNodeReadOnly(LNode, ANodesByName);
-		}
-	}
+        private static void UpdateNodeReadOnly(XmlNode ANode, Hashtable ANodesByName)
+        {
+            XmlAttribute LName = ANode.Attributes[Persistence.CXmlBOPName];
+            if (LName != null)
+            {
+                var LDesignerNode = (DesignerNode) ANodesByName[LName.Value];
+                if (LDesignerNode != null)
+                    LDesignerNode.SetReadOnly(true, false);
+            }
+            foreach (XmlNode LNode in ANode.ChildNodes)
+                UpdateNodeReadOnly(LNode, ANodesByName);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FComponents = new System.ComponentModel.Container();
+            this.Size = new System.Drawing.Size(300, 300);
+            this.Text = "CustomFormDesigner";
+        }
+
+        #endregion
+    }
 }
