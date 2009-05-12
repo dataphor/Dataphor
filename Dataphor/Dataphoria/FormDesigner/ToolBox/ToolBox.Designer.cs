e07b6fcf3c2b8b3b92c3f82ae10f8346055dbf22
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-05-12 00:31:01 +0000

    copied image lists
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@201 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/Dataphoria/FormDesigner/ToolBox/ToolBox.Designer.cs
+++ Dataphor/Dataphoria/FormDesigner/ToolBox/ToolBox.Designer.cs
@@ -6,7 +6,6 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.ImageList FPointerImageList;
 
         /// <summary> 
         /// Clean up any resources being used.
@@ -29,18 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToolBox));
+            this.FNodesImageList = new System.Windows.Forms.ImageList(this.components);
             this.FPointerImageList = new System.Windows.Forms.ImageList(this.components);
-
+            this.SuspendLayout();
+            // 
+            // FNodesImageList
+            // 
+            this.FNodesImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("FNodesImageList.ImageStream")));
+            this.FNodesImageList.TransparentColor = System.Drawing.Color.LimeGreen;
+            this.FNodesImageList.Images.SetKeyName(0, "");
             // 
             // FPointerImageList
             // 
-            //this.FPointerImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("FPointerImageList.ImageStream")));
+            this.FPointerImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("FPointerImageList.ImageStream")));
             this.FPointerImageList.TransparentColor = System.Drawing.Color.Lime;
-            //this.FPointerImageList.Images.SetKeyName(0, "");
+            this.FPointerImageList.Images.SetKeyName(0, "");
+            // 
+            // ToolBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ToolBox";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList FNodesImageList;
+        private System.Windows.Forms.ImageList FPointerImageList;
     }
 }
