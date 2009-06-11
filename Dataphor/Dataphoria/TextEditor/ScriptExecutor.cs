ff554e0e2dbbddc816d98988f4124055fee91e37
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-06-11 13:19:31 +0000

    Script Executor on its own file
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@248 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/Dataphoria/TextEditor/ScriptExecutor.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Threading;
+using Alphora.Dataphor.DAE;
+using Alphora.Dataphor.DAE.Client;
+using Alphora.Dataphor.Frontend.Client.Windows;
+
+namespace Alphora.Dataphor.Dataphoria.TextEditor
+{
+    /// <summary> Asyncronously executes queries. </summary>
+    /// <remarks> Should not be used multiple times (create another instance). </remarks>
+    internal class ScriptExecutor : Object
+    {
+        public const int CStopTimeout = 10000; // ten seconds to synchronously stop
+        private Thread FAsyncThread;
+
+        private ExecuteFinishedHandler FExecuteFinished;
+        private ReportScriptProgressHandler FExecuteProgress;
+        private bool FIsRunning = false;
+        private IServerProcess FProcess;
+
+        private int FProcessID;
+        private string FScript;
+        private IServerSession FSession;
+
+        public ScriptExecutor(IServerSession ASession, String AScript, ReportScriptProgressHandler AExecuteProgress,
+                              ExecuteFinishedHandler AExecuteFinished)
+        {
+            FSession = ASession;
+            FScript = AScript;
+            FExecuteFinished = AExecuteFinished;
+            FExecuteProgress = AExecuteProgress;
+        }
+
+        public bool IsRunning
+        {
+            get { return FIsRunning; }
+        }
+
+        public void Start()
+        {
+            if (!FIsRunning)
+            {
+                CleanupProcess();
+                FProcess = FSession.StartProcess(new ProcessInfo(FSession.SessionInfo));
+                FProcessID = FProcess.ProcessID;
+                FIsRunning = true;
+                FAsyncThread = new Thread(ExecuteAsync);
+                FAsyncThread.Start();
+            }
+        }
+
+        private void CleanupProcess()
+        {
+            if (FProcess != null)
+            {
+                try
+                {
+                    FSession.StopProcess(FProcess);
+                }
+                catch
+                {
+                    // Don't rethrow, the session may have already been stopped
+                }
+                FProcess = null;
+                FProcessID = 0;
+            }
+        }
+
+        public void Stop()
+        {
+            if (FIsRunning)
+            {
+                FIsRunning = false;
+
+                // Asyncronously request that the server process be stopped
+                new AsyncStopHandler(AsyncStop).BeginInvoke(FProcessID, FSession, null, null);
+                FProcess = null;
+                FProcessID = 0;
+            }
+        }
+
+        private void AsyncStop(int AProcessID, IServerSession ASession)
+        {
+            IServerProcess LProcess = ASession.StartProcess(new ProcessInfo(FSession.SessionInfo));
+            try
+            {
+                LProcess.Execute("StopProcess(" + AProcessID + ")", null);
+            }
+            finally
+            {
+                ASession.StopProcess(LProcess);
+            }
+        }
+
+        private void ExecuteAsync()
+        {
+            try
+            {
+                ErrorList LErrors = null;
+                TimeSpan LElapsed = TimeSpan.Zero;
+                
+
+                try
+                {
+                    ScriptExecutionUtility.ExecuteScript
+                        (
+                        FProcess,
+                        FScript,
+                        ScriptExecuteOption.All,
+                        out LErrors,
+                        out LElapsed,
+                        (AStatistics, AResults) => Session.SafelyInvoke(new ReportScriptProgressHandler(AsyncProgress),
+                                                                        new object[] {AStatistics, AResults})
+                        );
+                }
+                finally
+                {
+                    Session.SafelyInvoke(new ExecuteFinishedHandler(AsyncFinish), new object[] {LErrors, LElapsed});
+                }
+            }
+            catch
+            {
+                // Don't allow exceptions to go unhandled... the framework will abort the application
+            }
+        }
+
+        private void AsyncProgress(PlanStatistics AStatistics, string AResults)
+        {
+            // Return what results we got even if stopped.
+            FExecuteProgress(AStatistics, AResults);
+        }
+
+        private void AsyncFinish(ErrorList AErrors, TimeSpan AElapsedTime)
+        {
+            CleanupProcess();
+            if (FIsRunning)
+                FExecuteFinished(AErrors, AElapsedTime);
+        }
+
+        #region Nested type: AsyncStopHandler
+
+        private delegate void AsyncStopHandler(int AProcessID, IServerSession ASession);
+
+        #endregion
+    }
+
+    [Flags]
+    public enum ExportType
+    {
+        Data = 1,
+        Schema = 2
+    }
+
+    internal delegate void ExecuteFinishedHandler(ErrorList AErrors, TimeSpan AElapsedTime);
+}
