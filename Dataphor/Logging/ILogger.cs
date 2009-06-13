4e7dcc98ffd89efc6c8cb331498236f71049f9c9
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-06-13 05:39:24 +0000

    optimized imports
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@255 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/Logging/ILogger.cs
+++ Dataphor/Logging/ILogger.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
+
 
 namespace Alphora.Dataphor.Logging
 {
--- Dataphor/Logging/Logger.cs
+++ Dataphor/Logging/Logger.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Diagnostics;
-using System.Text;
+
 
 namespace Alphora.Dataphor.Logging
 {
