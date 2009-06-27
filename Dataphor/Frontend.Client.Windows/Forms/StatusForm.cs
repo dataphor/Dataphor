52808ba30f2cd25b16d575d061d11cf07b934faf
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-06-27 23:39:00 +0000

    new cogwheel icon for StatusForm
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@311 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/Frontend.Client.Windows/Forms/StatusForm.cs
+++ Dataphor/Frontend.Client.Windows/Forms/StatusForm.cs
@@ -53,48 +53,48 @@ namespace Alphora.Dataphor.Frontend.Client.Windows
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StatusForm));
-			this.panel = new System.Windows.Forms.Panel();
-			this.label = new System.Windows.Forms.Label();
-			this.panel.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// panel
-			// 
-			this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.panel.Controls.Add(this.label);
-			this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panel.Location = new System.Drawing.Point(0, 0);
-			this.panel.Name = "panel";
-			this.panel.Size = new System.Drawing.Size(456, 62);
-			this.panel.TabIndex = 0;
-			// 
-			// label
-			// 
-			this.label.Image = ((System.Drawing.Image)(resources.GetObject("label.Image")));
-			this.label.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-			this.label.Location = new System.Drawing.Point(2, 13);
-			this.label.Name = "label";
-			this.label.Size = new System.Drawing.Size(448, 32);
-			this.label.TabIndex = 1;
-			this.label.Text = "Processing...";
-			this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// StatusForm
-			// 
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.ClientSize = new System.Drawing.Size(456, 62);
-			this.Controls.Add(this.panel);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "StatusForm";
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "StatusForm";
-			this.panel.ResumeLayout(false);
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StatusForm));
+            this.panel = new System.Windows.Forms.Panel();
+            this.label = new System.Windows.Forms.Label();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel.Controls.Add(this.label);
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(456, 62);
+            this.panel.TabIndex = 0;
+            // 
+            // label
+            // 
+            this.label.Image = global::Alphora.Dataphor.Frontend.Client.Windows.General.cogwheel;
+            this.label.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label.Location = new System.Drawing.Point(3, 4);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(448, 52);
+            this.label.TabIndex = 1;
+            this.label.Text = "Processing...";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // StatusForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(456, 62);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StatusForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StatusForm";
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
 
 		}
 		#endregion
