3e503971933a549a675afa26f1f3d30d04103730
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-07-05 07:28:28 +0000

    Enhanced test
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@351 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Libraries/TestFramework.Coverage.Base/Source/SQLStoreTest.cs
+++ Libraries/TestFramework.Coverage.Base/Source/SQLStoreTest.cs
@@ -18,15 +18,28 @@ namespace Alphora.Dataphor.DAE.Diagnostics
         {
             SQLStore LSQLStore;
             LSQLStore = new SQLCEStore();
-            LSQLStore.ConnectionString = @"Data Source=E:\Users\Luxspes\Documents\Visual Studio 2008\SqlCE\MyDatabase#1.sdf";
+            LSQLStore.ConnectionString = @"Data Source=E:\Users\Luxspes\Documents\Visual Studio 2008\SqlCE\MyDatabase1.sdf";
             SQLStoreConnection LConnection = LSQLStore.Connect();
             string ATableName = "TableTest";
             SQLIndex AIndex = new SQLIndex("PK_TableTest", new[] { new SQLIndexColumn("ID") });
-            
-            SQLStoreCursor LCursor = LConnection.OpenCursor(ATableName, AIndex, true);
+
             //The table has 2 columns ID (integer) and NAME (nvarchar 50)
+
+            LConnection.BeginTransaction(System.Data.IsolationLevel.ReadCommitted);
+
+            SQLStoreCursor LCursor = LConnection.OpenCursor(ATableName, AIndex, true);
             var LRow = new object[] { 1, "Hi" };
             LCursor.Insert(LRow);
+            LCursor.Dispose();
+
+            LCursor = LConnection.OpenCursor(ATableName, AIndex, true);
+            LRow = new object[] {2, "Bye" };
+            LCursor.Insert(LRow);
+
+            LCursor.SetRange(null, null);
+
+            LCursor.Dispose();
+            LConnection.CommitTransaction();
         }
 
         //Data Source=
@@ -40,11 +53,24 @@ namespace Alphora.Dataphor.DAE.Diagnostics
             SQLStoreConnection LConnection = LSQLStore.Connect();
             string ATableName = "TableTest";
             SQLIndex AIndex = new SQLIndex("PK_TableTest", new[] { new SQLIndexColumn("ID") });
-            
-            SQLStoreCursor LCursor = LConnection.OpenCursor(ATableName, AIndex, true);
+
             //The table has 2 columns ID (integer) and NAME (nvarchar 50)
+
+            LConnection.BeginTransaction(System.Data.IsolationLevel.ReadCommitted);
+
+            SQLStoreCursor LCursor = LConnection.OpenCursor(ATableName, AIndex, true);            
             var LRow=new object[]{1,"Hi"};
             LCursor.Insert(LRow);
+            LCursor.Dispose();
+
+            LCursor = LConnection.OpenCursor(ATableName, AIndex, true);
+            LRow = new object[] { 2, "Bye" };
+            LCursor.Insert(LRow);
+
+            LCursor.SetRange(null, null);
+
+            LCursor.Dispose();
+            LConnection.CommitTransaction();                                   
         }
     }
 }
