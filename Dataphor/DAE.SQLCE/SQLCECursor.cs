5889ecd609006796535ac4a5a8a1754e49e973e3
bryn <bryn@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-07-08 05:30:13 +0000

    Missed a file.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@358 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/DAE.SQLCE/SQLCECursor.cs
@@ -0,0 +1,25 @@
+﻿/*
+	Dataphor
+	© Copyright 2000-2008 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+
+using System;
+using System.Data;
+using System.Data.SqlServerCe;
+
+using Alphora.Dataphor.DAE.Connection;
+
+namespace Alphora.Dataphor.DAE
+{
+	public class SQLCECursor : DotNetCursor
+	{
+		public SQLCECursor(SQLCECommand ACommand, SqlCeResultSet AResultSet) : base(ACommand, AResultSet)
+		{
+			FResultSet = AResultSet;
+		}
+		
+		private SqlCeResultSet FResultSet;
+		public SqlCeResultSet ResultSet { get { return FResultSet; } }
+	}
+}
