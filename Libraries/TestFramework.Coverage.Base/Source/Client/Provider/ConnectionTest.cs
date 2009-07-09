7000be27531485ef0a5fd83fb4dcbc5a6b9cab5a
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-07-09 01:33:03 +0000

    -copy output of compilation to bin\Debug and to ..\ -new test for ADO.NET provider
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@366 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Libraries/TestFramework.Coverage.Base/Source/Client/Provider/ConnectionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alphora.Dataphor.DAE.Client.Provider;
+using NUnit.Framework;
+
+namespace Alphora.Dataphor.DAE.Diagnostics.Client.Provider
+{
+    [TestFixture]
+    public class ConnectionTest
+    {
+        
+        [Test]
+        public void OpenConnection()
+        {
+            DAEConnection daeConnection;
+            string connectionString="";
+            using(daeConnection = new DAEConnection(connectionString))
+            {
+                daeConnection.Open();
+            }
+        }
+
+    }
+}
