cb9fa2bb337b9ba3116d552127366aca7afc2c31
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-07-09 18:15:54 +0000

    initial test for ADO.NET API support passed
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@373 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Libraries/TestFramework.Coverage.Base/Source/Client/Provider/ConnectionTest.cs
+++ Libraries/TestFramework.Coverage.Base/Source/Client/Provider/ConnectionTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Text;
+using Alphora.Dataphor.DAE.Client;
 using Alphora.Dataphor.DAE.Client.Provider;
 using NUnit.Framework;
 
@@ -9,15 +11,24 @@ namespace Alphora.Dataphor.DAE.Diagnostics.Client.Provider
     [TestFixture]
     public class ConnectionTest
     {
-        
+       
+
         [Test]
         public void OpenConnection()
         {
-            DAEConnection daeConnection;
-            string connectionString="";
-            using(daeConnection = new DAEConnection(connectionString))
+            DAEConnection LConnection;
+            string connectionString = "LocalInstance";            
+            using(LConnection = new DAEConnection(connectionString))
             {
-                daeConnection.Open();
+                LConnection.Open();
+                using (DbCommand LCommand = LConnection.CreateCommand())
+                {
+                    LCommand.Connection = LConnection;
+                    LCommand.CommandText = "select  Libraries group add {Count() LibCount}";
+                    var LCount = LCommand.ExecuteScalar();
+                    Console.WriteLine(LCount.GetType());
+                    Console.WriteLine(LCount);
+                }
             }
         }
 
