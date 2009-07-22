1b56dc874e3d88450fab6ecdb5e2abb2e2866351
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-07-22 12:40:11 +0000

    PostgreSQL catalog support (still not ready)
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@426 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/DAE.PGSQL/PGSQLStoreConnection.cs
@@ -0,0 +1,45 @@
+/*
+	Alphora Dataphor
+	© Copyright 2000-2008 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+
+#define USESQLCONNECTION
+
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+#if USESQLCONNECTION
+using Alphora.Dataphor.DAE.Connection;
+#else
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+#endif
+
+namespace Alphora.Dataphor.DAE.Store.PGSQL
+{
+    public class PostgreSQLStoreConnection : SQLStoreConnection
+    {
+        public PostgreSQLStoreConnection(PostgreSQLStore AStore) : base(AStore) { }
+
+		#if USESQLCONNECTION
+		protected override SQLConnection InternalCreateConnection()
+		{
+			return new MSSQLConnection(Store.ConnectionString);
+		}
+		#else
+        protected override DbConnection InternalCreateConnection()
+        {
+            return new SqlConnection(Store.ConnectionString);
+        }
+        #endif
+
+        public override bool HasTable(string ATableName)
+        {
+            return ((int)this.ExecuteScalar(String.Format("select count(*) from INFORMATION_SCHEMA.TABLES where TABLE_NAME = '{0}'", ATableName)) != 0);
+        }
+    }
+}
