96b7fb03a24a78f9618a8359ec1806b65dc7dc58
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-07-24 01:49:28 +0000

    InstructionNodes in their own file
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@441 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Libraries/MSSQLDevice/Source/Device.MSSQL/MSSQLDevice.cs
+++ Libraries/MSSQLDevice/Source/Device.MSSQL/MSSQLDevice.cs
@@ -18,15 +18,13 @@ using Alphora.Dataphor.DAE.Language;
 using Alphora.Dataphor.DAE.Language.D4;
 using Alphora.Dataphor.DAE.Language.SQL;
 using Alphora.Dataphor.DAE.Language.TSQL;
-using Alphora.Dataphor.DAE.Runtime;
-using Alphora.Dataphor.DAE.Runtime.Data;
+
 using Alphora.Dataphor.DAE.Runtime.Instructions;
 using Alphora.Dataphor.DAE.Schema;
 using Alphora.Dataphor.DAE.Server;
 using ColumnExpression=Alphora.Dataphor.DAE.Language.SQL.ColumnExpression;
 using DropIndexStatement=Alphora.Dataphor.DAE.Language.TSQL.DropIndexStatement;
 using SelectStatement=Alphora.Dataphor.DAE.Language.SQL.SelectStatement;
-using Alphora.Dataphor.DAE.Streams;
 
 namespace Alphora.Dataphor.DAE.Device.MSSQL
 {
@@ -1285,129 +1283,5 @@ if not exists (select * from sysdatabases where name = '{0}')
 
     #endregion
 
-    #region Instruction Nodes
-
-    public class MSSQLMSSQLBinaryCompareNode : BinaryInstructionNode
-    {
-        public static int Compare(ServerProcess AProcess, object ALeftValue, object ARightValue)
-        {
-            Stream LLeftStream = 
-				ALeftValue is byte[]
-					? new MemoryStream((byte[])ALeftValue, 0, ((byte[])ALeftValue).Length, false, true)
-					: AProcess.StreamManager.Open((StreamID)ALeftValue, LockMode.Exclusive);
-            try
-            {
-                Stream LRightStream = 
-					ARightValue is byte[]
-						? new MemoryStream((byte[])ARightValue, 0, ((byte[])ARightValue).Length, false, true)
-						: AProcess.StreamManager.Open((StreamID)ARightValue, LockMode.Exclusive);
-                try
-                {
-                    int LLeftByte;
-                    int LRightByte;
-
-                    while (true)
-                    {
-                        LLeftByte = LLeftStream.ReadByte();
-                        LRightByte = LRightStream.ReadByte();
-
-                        if (LLeftByte != LRightByte)
-                            break;
-
-                        if (LLeftByte == -1)
-                            break;
-
-                        if (LRightByte == -1)
-                            break;
-                    }
-
-                    return LLeftByte == LRightByte ? 0 : LLeftByte > LRightByte ? 1 : -1;
-                }
-                finally
-                {
-                    LRightStream.Close();
-                }
-            }
-            finally
-            {
-                LLeftStream.Close();
-            }
-        }
-
-        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
-        {
-            if ((AArgument1 == null) || (AArgument2 == null))
-                return null;
-            else
-                return Compare(AProcess, AArgument1, AArgument2);
-        }
-    }
-
-    public class MSSQLMSSQLBinaryEqualNode : BinaryInstructionNode
-    {
-        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
-        {
-            if ((AArgument1 == null) || (AArgument2 == null))
-                return null;
-            else
-                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) == 0;
-        }
-    }
-
-    public class MSSQLMSSQLBinaryNotEqualNode : BinaryInstructionNode
-    {
-        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
-        {
-            if ((AArgument1 == null) || (AArgument2 == null))
-                return null;
-            else
-                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) != 0;
-        }
-    }
-
-    public class MSSQLMSSQLBinaryLessNode : BinaryInstructionNode
-    {
-        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
-        {
-            if ((AArgument1 == null) || (AArgument2 == null))
-                return null;
-            else
-                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) < 0;
-        }
-    }
-
-    public class MSSQLMSSQLBinaryInclusiveLessNode : BinaryInstructionNode
-    {
-        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
-        {
-            if ((AArgument1 == null) || (AArgument2 == null))
-                return null;
-            else
-                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) <= 0;
-        }
-    }
-
-    public class MSSQLMSSQLBinaryGreaterNode : BinaryInstructionNode
-    {
-        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
-        {
-            if ((AArgument1 == null) || (AArgument2 == null))
-                return null;
-            else
-                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) > 0;
-        }
-    }
-
-    public class MSSQLMSSQLBinaryInclusiveGreaterNode : BinaryInstructionNode
-    {
-        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
-        {
-            if ((AArgument1 == null) || (AArgument2 == null))
-                return null;
-            else
-                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) >= 0;
-        }
-    }
-
-    #endregion
+    
 }
---
+++ Libraries/MSSQLDevice/Source/Device.MSSQL/MSSQLInstructionNodes.cs
@@ -0,0 +1,149 @@
+﻿/*
+	Alphora Dataphor
+	© Copyright 2000-2008 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+
+#define USECONNECTIONPOOLING
+#define USESQLOLEDB
+//#define USEOLEDBCONNECTION
+//#define USEADOCONNECTION
+
+
+using System.IO;
+using Alphora.Dataphor.DAE.Runtime;
+
+using Alphora.Dataphor.DAE.Runtime.Instructions;
+
+using Alphora.Dataphor.DAE.Server;
+using Alphora.Dataphor.DAE.Streams;
+
+namespace Alphora.Dataphor.DAE.Device.MSSQL
+{
+
+
+
+    public class MSSQLMSSQLBinaryCompareNode : BinaryInstructionNode
+    {
+        public static int Compare(ServerProcess AProcess, object ALeftValue, object ARightValue)
+        {
+            Stream LLeftStream =
+                ALeftValue is byte[]
+                    ? new MemoryStream((byte[])ALeftValue, 0, ((byte[])ALeftValue).Length, false, true)
+                    : AProcess.StreamManager.Open((StreamID)ALeftValue, LockMode.Exclusive);
+            try
+            {
+                Stream LRightStream =
+                    ARightValue is byte[]
+                        ? new MemoryStream((byte[])ARightValue, 0, ((byte[])ARightValue).Length, false, true)
+                        : AProcess.StreamManager.Open((StreamID)ARightValue, LockMode.Exclusive);
+                try
+                {
+                    int LLeftByte;
+                    int LRightByte;
+
+                    while (true)
+                    {
+                        LLeftByte = LLeftStream.ReadByte();
+                        LRightByte = LRightStream.ReadByte();
+
+                        if (LLeftByte != LRightByte)
+                            break;
+
+                        if (LLeftByte == -1)
+                            break;
+
+                        if (LRightByte == -1)
+                            break;
+                    }
+
+                    return LLeftByte == LRightByte ? 0 : LLeftByte > LRightByte ? 1 : -1;
+                }
+                finally
+                {
+                    LRightStream.Close();
+                }
+            }
+            finally
+            {
+                LLeftStream.Close();
+            }
+        }
+
+        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
+        {
+            if ((AArgument1 == null) || (AArgument2 == null))
+                return null;
+            else
+                return Compare(AProcess, AArgument1, AArgument2);
+        }
+    }
+
+    public class MSSQLMSSQLBinaryEqualNode : BinaryInstructionNode
+    {
+        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
+        {
+            if ((AArgument1 == null) || (AArgument2 == null))
+                return null;
+            else
+                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) == 0;
+        }
+    }
+
+    public class MSSQLMSSQLBinaryNotEqualNode : BinaryInstructionNode
+    {
+        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
+        {
+            if ((AArgument1 == null) || (AArgument2 == null))
+                return null;
+            else
+                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) != 0;
+        }
+    }
+
+    public class MSSQLMSSQLBinaryLessNode : BinaryInstructionNode
+    {
+        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
+        {
+            if ((AArgument1 == null) || (AArgument2 == null))
+                return null;
+            else
+                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) < 0;
+        }
+    }
+
+    public class MSSQLMSSQLBinaryInclusiveLessNode : BinaryInstructionNode
+    {
+        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
+        {
+            if ((AArgument1 == null) || (AArgument2 == null))
+                return null;
+            else
+                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) <= 0;
+        }
+    }
+
+    public class MSSQLMSSQLBinaryGreaterNode : BinaryInstructionNode
+    {
+        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
+        {
+            if ((AArgument1 == null) || (AArgument2 == null))
+                return null;
+            else
+                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) > 0;
+        }
+    }
+
+    public class MSSQLMSSQLBinaryInclusiveGreaterNode : BinaryInstructionNode
+    {
+        public override object InternalExecute(ServerProcess AProcess, object AArgument1, object AArgument2)
+        {
+            if ((AArgument1 == null) || (AArgument2 == null))
+                return null;
+            else
+                return MSSQLMSSQLBinaryCompareNode.Compare(AProcess, AArgument1, AArgument2) >= 0;
+        }
+    }
+
+ 
+}
