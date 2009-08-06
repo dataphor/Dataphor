30f8213d5dedc4be57845f04ea527d2fd6ed978f
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-08-06 01:37:48 +0000

    removed some stuff not related to PGSQL
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@487 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Libraries/DAE.DevicePGSQL/Source/Device.PGSQL/PostgreSQLConnectionStringBuilder.cs
+++ Libraries/DAE.DevicePGSQL/Source/Device.PGSQL/PostgreSQLConnectionStringBuilder.cs
@@ -6,52 +6,24 @@ using Alphora.Dataphor.DAE.Language.D4;
 namespace Alphora.Dataphor.Device.PGSQL
 {
 
-	/// <summary>
-	/// This class is the tag translator for ADO
-	/// </summary>
 	public class PostgreSQLOLEDBConnectionStringBuilder : ConnectionStringBuilder
 	{
 		public PostgreSQLOLEDBConnectionStringBuilder()
 		{
-#if USESQLOLEDB
-            FParameters.AddOrUpdate("Provider", "SQLOLEDB");
-#else
-			FParameters.AddOrUpdate("Provider", "MSDASQL");
-#endif
-
-#if !USECONNECTIONPOOLING
-			FParameters.AddOrUpdate("OLE DB Services", "-2"); // Turn off OLEDB resource pooling
-#endif
-			FLegend.AddOrUpdate("ServerName", "Data source");
-			FLegend.AddOrUpdate("DatabaseName", "initial catalog");
+			FParameters.AddOrUpdate("Provider", "MSDAORA");
+			FLegend.AddOrUpdate("HostName", "Data source");
 			FLegend.AddOrUpdate("UserName", "user id");
 			FLegend.AddOrUpdate("Password", "password");
-			FLegend.AddOrUpdate("ApplicationName", "app name");
-		}
-
-		public override Tags Map(Tags ATags)
-		{
-			Tags LTags = base.Map(ATags);
-			Tag LTag = LTags.GetTag("IntegratedSecurity");
-			if (LTag != null)
-			{
-				LTags.Remove(LTag);
-				LTags.AddOrUpdate("Integrated Security", "SSPI");
-			}
-			return LTags;
 		}
 	}
 
-	public class PostgreSQLADODotNetConnectionStringBuilder : ConnectionStringBuilder
+	// for the PostgreSQL.NET data provider
+	public class PostgreSQLConnectionStringBuilder : ConnectionStringBuilder
 	{
-		public PostgreSQLADODotNetConnectionStringBuilder()
+		public PostgreSQLConnectionStringBuilder()
 		{
-			FLegend.AddOrUpdate("ServerName", "server");
-			FLegend.AddOrUpdate("DatabaseName", "database");
-			FLegend.AddOrUpdate("UserName", "user id");
-			FLegend.AddOrUpdate("Password", "password");
-			FLegend.AddOrUpdate("IntegratedSecurity", "integrated security");
-			FLegend.AddOrUpdate("ApplicationName", "application name");
+			FLegend.AddOrUpdate("HostName", "Data Source");
+			FLegend.AddOrUpdate("UserName", "User Id");
 		}
 	}
 
@@ -59,32 +31,9 @@ namespace Alphora.Dataphor.Device.PGSQL
 	{
 		public PostgreSQLODBCConnectionStringBuilder()
 		{
-			FLegend.AddOrUpdate("ServerName", "DSN");
-			FLegend.AddOrUpdate("DatabaseName", "Database");
+			FLegend.AddOrUpdate("HostName", "DSN");
 			FLegend.AddOrUpdate("UserName", "UID");
 			FLegend.AddOrUpdate("Password", "PWD");
-			FLegend.AddOrUpdate("ApplicationName", "APPNAME");
-		}
-
-		public override Tags Map(Tags ATags)
-		{
-			Tags LTags = base.Map(ATags);
-			Tag LTag = LTags.GetTag("IntegratedSecurity");
-			if (LTag != null)
-			{
-				LTags.Remove(LTag.Name);
-				LTags.AddOrUpdate("Trusted_Connection", "Yes");
-			}
-			return LTags;
-		}
-	}
-
-	public class AccessConnectionStringBuilder : ConnectionStringBuilder
-	{
-		public override Tags Map(Tags ATags)
-		{
-			Tags LTags = base.Map(ATags);
-			return LTags;
 		}
 	}
 
