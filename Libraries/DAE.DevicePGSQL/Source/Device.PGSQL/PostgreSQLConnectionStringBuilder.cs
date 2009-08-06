76a0281e21c63bdb504a01ad87935de40d94ee83
luxspes <luxspes@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-08-06 01:48:20 +0000

    for the moment it will only support the PostgreSQL.NET data provider
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@488 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Libraries/DAE.DevicePGSQL/Source/Device.PGSQL/PostgreSQLConnectionStringBuilder.cs
+++ Libraries/DAE.DevicePGSQL/Source/Device.PGSQL/PostgreSQLConnectionStringBuilder.cs
@@ -5,35 +5,17 @@ using Alphora.Dataphor.DAE.Language.D4;
 
 namespace Alphora.Dataphor.Device.PGSQL
 {
-
-	public class PostgreSQLOLEDBConnectionStringBuilder : ConnectionStringBuilder
-	{
-		public PostgreSQLOLEDBConnectionStringBuilder()
-		{
-			FParameters.AddOrUpdate("Provider", "MSDAORA");
-			FLegend.AddOrUpdate("HostName", "Data source");
-			FLegend.AddOrUpdate("UserName", "user id");
-			FLegend.AddOrUpdate("Password", "password");
-		}
-	}
-
+	
 	// for the PostgreSQL.NET data provider
 	public class PostgreSQLConnectionStringBuilder : ConnectionStringBuilder
 	{
 		public PostgreSQLConnectionStringBuilder()
 		{
-			FLegend.AddOrUpdate("HostName", "Data Source");
+			FLegend.AddOrUpdate("HostName", "Server");
 			FLegend.AddOrUpdate("UserName", "User Id");
-		}
-	}
-
-	public class PostgreSQLODBCConnectionStringBuilder : ConnectionStringBuilder
-	{
-		public PostgreSQLODBCConnectionStringBuilder()
-		{
-			FLegend.AddOrUpdate("HostName", "DSN");
-			FLegend.AddOrUpdate("UserName", "UID");
-			FLegend.AddOrUpdate("Password", "PWD");
+			FLegend.AddOrUpdate("Password", "User Id");
+			FLegend.AddOrUpdate("Database", "Database");
+			FLegend.AddOrUpdate("SearchPath", "SearchPath");			
 		}
 	}
 
