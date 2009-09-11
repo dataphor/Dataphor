0d353fdf7e94a9b1aca79065a35520050cfa0a11
bryn <bryn@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-09-11 18:49:44 +0000

    Refactoring Engine to remove non-Silverlight supported dependencies.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@690 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/DAE/Schema/ServerLink.cs
+++ Dataphor/DAE/Schema/ServerLink.cs
@@ -21,7 +21,6 @@ using Alphora.Dataphor.DAE.Language.D4;
 using Alphora.Dataphor.DAE.Runtime;
 using Alphora.Dataphor.DAE.Runtime.Data;
 using Alphora.Dataphor.DAE.Runtime.Instructions;
-using Alphora.Dataphor.DAE.NativeCLI;
 
 namespace Alphora.Dataphor.DAE.Schema
 {
