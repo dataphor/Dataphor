0290655a688dc20d9c602cdfb858e09a2d8ffa31
n8allan <n8allan@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-09-26 18:33:50 +0000

    ReaderWriterLock with timeouts.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@779 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/Base.Silverlight/System/ReaderWriterLock.cs
+++ Dataphor/Base.Silverlight/System/ReaderWriterLock.cs
@@ -2,61 +2,74 @@
 
 namespace System.Threading
 {
+	/// <remarks>
+	///		Adapted from: http://stackoverflow.com/questions/612765/silverlight-readerwriterlock-implementation-good-bad
+	/// </remarks>
 	public sealed class ReaderWriterLock
 	{
-		private readonly object internalLock = new object();
-		private int activeReaders = 0;
-		private bool activeWriter = false;
+		private readonly object FInternalLock = new Object();
+		private int FActiveReaders = 0;
+		private bool FActiveWriter = false;
+		
+		public void AcquireReaderLock(int ATimeout)
+		{
+			lock (FInternalLock)
+			{
+				while (FActiveWriter)
+					if (!Monitor.Wait(FInternalLock, ATimeout))
+						throw new Exception("Timeout waiting for reader lock.");
+				++FActiveReaders;
+			}
+		}
 
 		public void AcquireReaderLock()
 		{
-			lock (internalLock)
-			{
-				while (activeWriter)
-					Monitor.Wait(internalLock);
-				++activeReaders;
-			}
+			AcquireReaderLock(Timeout.Infinite);
 		}
 
 		public void ReleaseReaderLock()
 		{
-			lock (internalLock)
+			lock (FInternalLock)
+			{            
+				// TODO: if FActiveReaders <= 0 do some error handling
+				--FActiveReaders;
+				Monitor.PulseAll(FInternalLock);
+			}
+		}
+		
+		public void AcquireWriterLock(int ATimeout)
+		{
+			lock (FInternalLock)
 			{
-				// if activeReaders <= 0 do some error handling
-				--activeReaders;
-				Monitor.PulseAll(internalLock);
+				// First wait for any writers to clear             
+				// This assumes writers have a higher priority than readers            
+				// as it will force the readers to wait until all writers are done.            
+				// you can change the conditionals in here to change that behavior.            
+				while (FActiveWriter)
+					if (!Monitor.Wait(FInternalLock, ATimeout))
+						throw new Exception("Timeout waiting for writer lock.");
+				// There are no more writers, set this to true to block further readers from acquiring the lock            
+				FActiveWriter = true;				
+				// Now wait till all readers have completed.            
+				while (FActiveReaders > 0)
+					if (!Monitor.Wait(FInternalLock, ATimeout))
+						throw new Exception("Timeout waiting for writer lock.");
+				// The writer now has the lock
 			}
 		}
 
 		public void AcquireWriterLock()
 		{
-			lock (internalLock)
-			{
-				// first wait for any writers to clear 
-				// This assumes writers have a higher priority than readers
-				// as it will force the readers to wait until all writers are done.
-				// you can change the conditionals in here to change that behavior.
-				while (activeWriter)
-					Monitor.Wait(internalLock);
-
-				// There are no more writers, set this to true to block further readers from acquiring the lock
-				activeWriter = true;
-
-				// Now wait till all readers have completed.
-				while (activeReaders > 0)
-					Monitor.Wait(internalLock);
-
-				// The writer now has the lock
-			}
+			AcquireWriterLock(Timeout.Infinite);
 		}
-
+		
 		public void ReleaseWriterLock()
 		{
-			lock (internalLock)
+			lock (FInternalLock)
 			{
-				// if activeWriter != true handle the error
-				activeWriter = false;
-				Monitor.PulseAll(internalLock);
+				// if activeWriter != true handle the error            
+				FActiveWriter = false;
+				Monitor.PulseAll(FInternalLock);
 			}
 		}
 	}
