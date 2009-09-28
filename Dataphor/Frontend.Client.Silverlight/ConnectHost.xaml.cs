74b530633a608b28fe993cb62fc7f46b612f0329
n8allan <n8allan@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-09-28 21:11:27 +0000

    Rebuild of silverlight client.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@793 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/Frontend.Client.Silverlight/ConnectHost.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+using Alphora.Dataphor.DAE.Listener;
+
+namespace Frontend.Client.Silverlight
+{
+    public partial class ConnectHost : Page
+    {
+        public ConnectHost()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+			NavigationService.Navigate
+			(
+				new Uri
+				(
+					"ConnectInstances.xaml?HostName=" + Uri.EscapeUriString(HostNameTextBox.Text), 
+					UriKind.Relative
+				)
+			);
+        }
+    }
+}
---
+++ Dataphor/Frontend.Client.Silverlight/ConnectInstances.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+using Alphora.Dataphor.DAE.Listener;
+
+namespace Frontend.Client.Silverlight
+{
+    public partial class ConnectInstances : Page, INotifyPropertyChanged
+    {
+        public ConnectInstances()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+			string LHostName;
+			if (NavigationContext.QueryString.TryGetValue("HostName", out LHostName))
+				HostName = LHostName;
+        }
+		
+		private string FHostName;
+		
+		public string HostName
+		{
+			get { return FHostName; }
+			set
+			{
+				if (FHostName != value)
+				{
+					FHostName = value;
+					
+					// Asynchronously load the instances
+					new Thread
+					(
+						new ThreadStart
+						(
+							delegate
+							{
+								var LInstances = ListenerFactory.EnumerateInstances(value);
+								Dispatcher.BeginInvoke
+								(
+									delegate 
+									{
+										if (FHostName == value)
+										{
+											if (LInstances.Length == 0)
+												NavigationService.Navigate(new Uri("ConnectHost.xaml"));
+											else
+												Instances = LInstances;
+										}
+									}
+								);
+							}
+						)
+					).Start();
+				}
+			}
+		}
+				
+		private string[] FInstances;
+		
+		public string[] Instances
+		{
+			get { return FInstances; }
+			set 
+			{ 
+				if (FInstances != value)
+				{
+					FInstances = value; 
+					NotifyPropertyChanged("Instances");
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	
+		public void NotifyPropertyChanged(string AName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(AName));
+		}
+
+		private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			if (InstanceListBox.SelectedItem != null)
+				NavigationService.Navigate
+				(
+					new Uri
+					(
+						"ConnectLogin.xaml?HostName=" + Uri.EscapeUriString(FHostName) 
+							+ "&InstanceName=" + Uri.EscapeUriString((string)InstanceListBox.SelectedItem)
+					)
+				);
+		}  
+	}
+}
