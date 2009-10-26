cff644053b2e27825102528a30955f08151c20ff
n8allan <n8allan@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-10-26 18:43:01 +0000

    SL UI enhancements to bread-crumb.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@897 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/Frontend.Client.Silverlight/Frontend.Client.Silverlight/Helpers/FirstItemInvisibleConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Alphora.Dataphor.Frontend.Client.Silverlight
+{
+	/// <summary> Converts from a ListBoxItem to visibility (1st item = Collapsed). </summary>
+	public class FirstItemInvisibleConverter : IValueConverter
+	{
+		public object Convert(object AValue, Type ATargetType, object AParameter, CultureInfo ACulture)
+		{
+			if (ATargetType != typeof(Visibility))
+				throw new InvalidOperationException("The target of the NullToVisiblity converter must be of type Visibility.");
+
+			bool LIsInverted = false;
+			if (AParameter != null)
+				LIsInverted = System.Convert.ToBoolean(AParameter);
+
+			var LListBoxItem = (ListBoxItem)AValue;
+			var LListBox = SilverlightUtility.FindVisualParent(LListBoxItem, typeof(ListBox)) as ListBox;
+			if (LListBox != null)
+			{
+				var LIndex = LListBox.ItemContainerGenerator.IndexFromContainer(LListBoxItem);
+				return (AValue != null && LIndex == 0) ^ LIsInverted ? Visibility.Collapsed : Visibility.Visible;
+			}
+			return LIsInverted;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException("ConvertBack not implemented on NullToVisibilityConverter.");
+		}
+	}
+}
---
+++ Dataphor/Frontend.Client.Silverlight/Frontend.Client.Silverlight/Helpers/SilverlightUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Alphora.Dataphor.Frontend.Client.Silverlight
+{
+	public static class SilverlightUtility
+	{
+		public static DependencyObject FindVisualParent(DependencyObject AObject, Type AType)
+		{
+			var LParent = VisualTreeHelper.GetParent(AObject);
+			if (LParent != null)
+			{
+				if (AType.IsAssignableFrom(LParent.GetType()))
+					return LParent;
+				else
+					return FindVisualParent(LParent, AType);
+			}
+			else
+				return null;
+		}
+	}
+}
