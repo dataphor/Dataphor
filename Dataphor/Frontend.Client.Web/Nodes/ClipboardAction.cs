40227be53ce21e772af230e85f74ddab96e2adde
n8allan <n8allan@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2009-12-15 10:02:05 +0000

    Dummy clipboard actions for Web Client.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@970 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/Frontend.Client.Web/Nodes/ClipboardAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Alphora.Dataphor.Frontend.Client;
+
+namespace Alphora.Dataphor.Frontend.Client.Web
+{
+	public class CopyAction : Action, ICopyAction
+	{
+		protected override void InternalExecute(INode ASender, EventParams AParams)
+		{
+			// TODO: Implement
+		}
+	}
+
+	public class PasteAction : Action, IPasteAction
+	{
+		protected override void InternalExecute(INode ASender, EventParams AParams)
+		{
+			// TODO: Implement
+		}
+	}
+}
