96cbacd5d3269cc2b70560b48de63a3ea9e7ae46
bryn <bryn@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2010-03-09 07:32:07 +0000

    Added stand-alone DAE server hosting application.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@1049 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphor/DAEServer/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+	Dataphor
+	© Copyright 2000-2010 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+using System;
+using System.Text;
+
+namespace DAEServer
+{
+	using Alphora.Dataphor.DAE.Server;
+	using Alphora.Dataphor.DAE.Service;
+
+	public class Program
+	{
+		private static DataphorServiceHost LServiceHost;
+		
+		private static string GetInstanceName(string[] AArgs)
+		{
+			for (int i = 0; i < AArgs.Length - 1; i++)
+				if ((AArgs[i].ToLower() == "-name") || (AArgs[i].ToLower() == "-n"))
+					return AArgs[i + 1];
+					
+			return null;
+		}
+		
+		static void Main(string[] AArgs)
+		{
+			string LInstanceName = GetInstanceName(AArgs);
+			if (LInstanceName == null)
+				LInstanceName = Engine.CDefaultServerName;
+				
+			Console.WriteLine("Server starting...");
+			DataphorServiceHost LHost = new DataphorServiceHost();
+			LHost.InstanceName = LInstanceName;
+			LHost.Start();
+			try
+			{
+				Console.WriteLine("Server started.");
+				Console.ReadLine();
+			}
+			finally
+			{
+				Console.WriteLine("Server stopping...");
+				LHost.Stop();
+			}
+		}
+	}
+}
