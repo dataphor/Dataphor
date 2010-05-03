7c058155f8b8d6a06d3f267743413522fa7135e4
bryn <bryn@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2010-05-03 15:34:54 +0000

    Missed some files for Server.Tests.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@1090 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Tests/DAE.Server.Tests/CursorTest.cs
@@ -0,0 +1,80 @@
+﻿/*
+	Dataphor
+	© Copyright 2000-2010 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Alphora.Dataphor.DAE.Server.Tests
+{
+	[TestFixture]
+	public class CursorTest : InProcessTestFixture
+	{
+		[Test]
+		public void TestDeleteAtEOF()
+		{
+			ExecuteScript("create table TestDeleteAtEOF { ID : Integer, Name : String, key { ID } };");
+			ExecuteScript("insert row { 1 ID, 'Joe' Name } into TestDeleteAtEOF;");
+			ExecuteScript("insert row { 2 ID, 'John' Name } into TestDeleteAtEOF;");
+			
+			IServerCursor LCursor = Process.OpenCursor("select TestDeleteAtEOF browse by { ID } capabilities { navigable, backwardsnavigable, bookmarkable, searchable, updateable } isolation browse", null);
+			try
+			{
+				var LRow = LCursor.Plan.RequestRow();
+				try
+				{
+					LCursor.Next();
+					LCursor.Next();
+					LCursor.Delete();
+					if (LCursor.EOF() && !LCursor.Prior())
+						throw new Exception("Delete At EOF Failed");
+				}
+				finally
+				{
+					LCursor.Plan.ReleaseRow(LRow);
+				}
+			}
+			finally
+			{
+				Process.CloseCursor(LCursor);
+			}
+		}
+
+		[Test]
+		public void TestDeleteAtBOF()
+		{
+			ExecuteScript("create table TestDeleteAtBOF { ID : Integer, Name : String, key { ID } };");
+			ExecuteScript("insert row { 1 ID, 'Joe' Name } into TestDeleteAtBOF;");
+			ExecuteScript("insert row { 2 ID, 'John' Name } into TestDeleteAtBOF;");
+			
+			IServerCursor LCursor = Process.OpenCursor("select TestDeleteAtBOF browse by { ID } capabilities { navigable, backwardsnavigable, bookmarkable, searchable, updateable } isolation browse", null);
+			try
+			{
+				var LRow = LCursor.Plan.RequestRow();
+				try
+				{
+					LCursor.Last();
+					LCursor.Prior();
+					LCursor.Prior();
+					LCursor.Delete();
+					if (LCursor.BOF() && !LCursor.Next())
+						throw new Exception("Delete At BOF Failed");
+				}
+				finally
+				{
+					LCursor.Plan.ReleaseRow(LRow);
+				}
+			}
+			finally
+			{
+				Process.CloseCursor(LCursor);
+			}
+		}
+	}
+}
---
+++ Tests/DAE.Server.Tests/InProcessTestFixture.cs
@@ -0,0 +1,104 @@
+﻿/*
+	Dataphor
+	© Copyright 2000-2010 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Alphora.Dataphor.DAE.Server.Tests
+{
+	using Alphora.Dataphor.DAE.Server.Tests.Utilities;
+	
+	public abstract class InProcessTestFixture
+	{
+		private ServerConfigurationManager FServerConfigurationManager;
+		protected ServerConfigurationManager ServerConfigurationManager { get { return FServerConfigurationManager; } }
+
+		private ServerConfiguration FConfiguration;
+		protected ServerConfiguration Configuration { get { return FConfiguration; } }
+
+		private IServer FServer;
+		protected IServer Server { get { return FServer; } }
+		
+		private IServerSession FSession;
+		protected IServerSession Session { get { return FSession; } }
+		
+		private IServerProcess FProcess;
+		protected IServerProcess Process { get { return FProcess; } }
+		
+		[TestFixtureSetUp]
+		public void SetUpFixture()
+		{
+			FServerConfigurationManager = new SQLCEServerConfigurationManager();
+			FConfiguration = FServerConfigurationManager.GetTestConfiguration("TestInstance");
+			FServerConfigurationManager.ResetInstance();
+			FServer = FServerConfigurationManager.GetServer();
+			FServer.Start();
+			
+			IServerSession LSession = FServer.Connect(new SessionInfo("Admin", ""));
+			try
+			{
+				IServerProcess LProcess = LSession.StartProcess(new ProcessInfo(LSession.SessionInfo));
+				try
+				{
+					LProcess.ExecuteScript("EnsureLibraryRegistered('Frontend');");
+					LProcess.ExecuteScript("EnsureLibraryRegistered('TestFramework.Coverage.Base');");
+				}
+				finally
+				{
+					LSession.StopProcess(LProcess);
+				}
+			}
+			finally
+			{
+				FServer.Disconnect(LSession);
+			}
+		}
+		
+		[TestFixtureTearDown]
+		public void TearDownFixture()
+		{
+			FServer.Stop();
+			FServerConfigurationManager.ResetInstance();
+		}
+		
+		[SetUp]
+		public void SetUp()
+		{
+			FSession = FServer.Connect(new SessionInfo("Admin", ""));
+			FProcess = FSession.StartProcess(new ProcessInfo(FSession.SessionInfo));
+		}
+		
+		[TearDown]
+		public void TearDown()
+		{
+			if (FProcess != null)
+			{
+				FSession.StopProcess(FProcess);
+				FProcess = null;
+			}
+			
+			if (FSession != null)
+			{
+				FServer.Disconnect(FSession);
+				FSession = null;
+			}
+		}
+		
+		protected void ExecuteScript(string AScript)
+		{
+			FProcess.ExecuteScript(AScript);
+		}
+		
+		protected void ExecuteScript(string ALibraryName, string AScriptName)
+		{
+			FProcess.ExecuteScript(String.Format("ExecuteScript('{0}', '{1}');", ALibraryName, AScriptName));
+		}
+	}
+}
