bf0bd8fc23460b62e16c9ff7de9856a8e2794977
bryn <bryn@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2012-04-25 04:59:34 +0000

    Fixed several errors with the NativeCLI implementation that were not addressed as part of the WCF refactor.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@1210 c93b7781-d36b-0e4d-be4f-9d94f2a838be

--- Dataphor/DAE.NativeCLI/NativeStructures.cs
+++ Dataphor/DAE.NativeCLI/NativeStructures.cs
@@ -23,28 +23,67 @@ namespace Alphora.Dataphor.DAE.NativeCLI
 	}
 	
 	[DataContract]
-	public enum NativeIsolationLevel { Browse, CursorStability, Isolated };
+	public enum NativeIsolationLevel 
+	{ 
+		[EnumMember]
+		Browse, 
+		
+		[EnumMember]
+		CursorStability, 
+		
+		[EnumMember]
+		Isolated 
+	};
 	
 	[DataContract]
-	public enum NativeModifier { In, Var, Out }
+	public enum NativeModifier 
+	{ 
+		[EnumMember]
+		In, 
+		
+		[EnumMember]
+		Var, 
+		
+		[EnumMember]
+		Out 
+	}
 	
 	[DataContract]
 	public struct NativeParam
 	{
+		[DataMember]
 		public string Name;
+
+		[DataMember]
 		public string DataTypeName;
+
+		[DataMember]
 		public NativeModifier Modifier;
+
+		[DataMember]
 		public object Value;
 	}
 	
 	[DataContract]
-	public enum NativeExecutionOptions { Default, SchemaOnly }
+	public enum NativeExecutionOptions 
+	{ 
+		[EnumMember]
+		Default, 
+		
+		[EnumMember]
+		SchemaOnly 
+	}
 	
 	[DataContract]
 	public struct NativeExecuteOperation
 	{
+		[DataMember]
 		public string Statement;
+
+		[DataMember]
 		public NativeParam[] Params;
+
+		[DataMember]
 		public NativeExecutionOptions Options;
 	}
 	
@@ -54,7 +93,10 @@ namespace Alphora.Dataphor.DAE.NativeCLI
 	[DataContract]
 	public class NativeScalarValue : NativeValue
 	{
+		[DataMember]
 		public string DataTypeName;
+
+		[DataMember]
 		public object Value;
 	}
 	
@@ -67,35 +109,50 @@ namespace Alphora.Dataphor.DAE.NativeCLI
 	[DataContract]
 	public struct NativeColumn
 	{
+		[DataMember]
 		public string Name;
+
+		[DataMember]
 		public string DataTypeName;
 	}
 	
 	[DataContract]
 	public struct NativeKey
 	{
+		[DataMember]
 		public string[] KeyColumns;
 	}
 	
 	[DataContract]
 	public class NativeRowValue : NativeValue
 	{
+		[DataMember]
 		public NativeColumn[] Columns;
+
+		[DataMember]
 		public object[] Values;
 	}
 
 	[DataContract]
 	public class NativeTableValue : NativeValue
 	{
+		[DataMember]
 		public NativeColumn[] Columns;
+
+		[DataMember]
 		public NativeKey[] Keys;
+
+		[DataMember]
 		public object[][] Rows;
 	}
 	
 	[DataContract]
 	public class NativeResult
 	{
+		[DataMember]
 		public NativeParam[] Params;
+
+		[DataMember]
 		public NativeValue Value;
 	}
 }
