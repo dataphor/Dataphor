60748904da2bb12729a89886140e9f7df4004b7b
BobbyR <BobbyR@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2012-04-28 00:29:40 +0000

    Add FastoreDevice
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@1234 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Libraries/FastoreDevice/Source/FastoreDevice.cs
@@ -0,0 +1,58 @@
+﻿/*
+	Dataphor
+	© Copyright 2000-2008 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Specialized;
+
+using Alphora.Dataphor.BOP;
+using Alphora.Dataphor.DAE.Schema;
+using Alphora.Dataphor.DAE.Streams;
+using Alphora.Dataphor.DAE.Language;
+using Alphora.Dataphor.DAE.Language.D4;
+using Alphora.Dataphor.DAE.Compiling;
+using Alphora.Dataphor.DAE.Runtime;
+using Alphora.Dataphor.DAE.Runtime.Data;
+using Alphora.Dataphor.DAE.Runtime.Instructions;
+using Alphora.Dataphor.DAE.Server;
+using Alphora.Dataphor.DAE.Device.Memory;
+using Schema = Alphora.Dataphor.DAE.Schema;
+using Alphora.Dataphor.Windows;
+
+namespace Alphora.Dataphor.DAE.Device.Fastore
+{
+	public class FastoreDevice : MemoryDevice
+	{
+		protected override void InternalStart(ServerProcess process)
+		{
+			
+
+			base.InternalStart(process);
+		}
+
+		protected override void InternalStop(ServerProcess process)
+		{
+			
+			base.InternalStop(process);
+		}
+
+		// Session
+		protected override DeviceSession InternalConnect(ServerProcess serverProcess, DeviceSessionInfo deviceSessionInfo)
+		{
+			return new FastoreDeviceSession(this, serverProcess, deviceSessionInfo);
+		}
+	}
+
+	public class FastoreDeviceSession : MemoryDeviceSession
+	{
+		protected internal FastoreDeviceSession
+		(
+			Schema.Device device, 
+			ServerProcess serverProcess, 
+			Schema.DeviceSessionInfo deviceSessionInfo
+		) : base(device, serverProcess, deviceSessionInfo){}
+	}
+}
