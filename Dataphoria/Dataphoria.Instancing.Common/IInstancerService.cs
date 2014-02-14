5d7db190c9059d7547566281f881a7ff7f442b2d
bryn <bryn@c93b7781-d36b-0e4d-be4f-9d94f2a838be>
2014-02-14 16:19:38 +0000

    Adding clustering implementation for instancer and coordinator.
    git-svn-id: https://svn.alphora.com:8443/svn/Dataphor/trunk@1392 c93b7781-d36b-0e4d-be4f-9d94f2a838be

---
+++ Dataphoria/Dataphoria.Instancing.Common/IInstancerService.cs
@@ -0,0 +1,34 @@
+﻿/*
+	Alphora Dataphor
+	© Copyright 2000-2014 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Alphora.Dataphor.DAE;
+using Alphora.Dataphor.DAE.Server;
+
+namespace Alphora.Dataphor.Dataphoria.Instancing.Common
+{
+    public interface IInstancerService
+    {
+        string[] EnumerateInstances();
+
+        ServerConfiguration GetInstance(string instanceName);
+
+        void CreateInstance(ServerConfiguration instance);
+
+        void DeleteInstance(string instanceName);
+
+        void StartInstance(string instanceName);
+
+        void StopInstance(string instanceName);
+
+        void KillInstance(string instanceName);
+
+        ServerState GetInstanceStatus(string instanceName);
+    }
+}
---
+++ Dataphoria/Dataphoria.Instancing/Instancer.cs
@@ -0,0 +1,58 @@
+﻿/*
+	Alphora Dataphor
+	© Copyright 2000-2014 Alphora
+	This file is licensed under a modified BSD-license which can be found here: http://dataphor.org/dataphor_license.txt
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Alphora.Dataphor.DAE;
+using Alphora.Dataphor.DAE.Server;
+
+namespace Alphora.Dataphor.Dataphoria.Instancing
+{
+    public class Instancer
+    {
+        public IEnumerable<string> EnumerateInstances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServerConfiguration GetInstance(string instanceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateInstance(ServerConfiguration instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteInstance(string instanceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartInstance(string instanceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopInstance(string instanceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KillInstance(string instanceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServerState GetInstanceStatus(string instanceName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
