350b827ab44a6be0ccd761d4ce3e34d54f1834c4
Bobby Rose <bobby@databaseconsultinggroup.com>
2016-10-28 10:57:47 -0600

    REST changes

---
+++ Dataphor/DAE/REST/RESTResult.cs
@@ -0,0 +1,10 @@
+﻿using Alphora.Dataphor.DAE.Runtime;
+
+namespace Alphora.Dataphor.DAE.REST
+{
+	public class RESTResult
+	{
+		public DataParam[] Params { get; set; }
+		public object Value { get; set; }
+	}
+}
--- Dataphoria/Dataphoria.Web/Scripts/_references.js
+++ Dataphoria/Dataphoria.Web/Scripts/_references.js
@@ -5,6 +5,5 @@
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="jquery-ui-1.11.4.js" />
 /// <reference path="jquery-ui-1.11.4.min.js" />
-/// <reference path="knockout-2.2.1.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
