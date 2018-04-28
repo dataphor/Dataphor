89666e704bea326150f36121a3f662ee28dc774a
JPercival <jonathan.i.percival@gmail.com>
2018-04-28 14:02:00 -0600

    Upgrade to 4.7.1

--- Dataphoria/Dataphoria.Instancer/ServiceSettings.Designer.cs
+++ Dataphoria/Dataphoria.Instancer/ServiceSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace Alphora.Dataphor.Dataphoria.Instancer {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     internal sealed partial class ServiceSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static ServiceSettings defaultInstance = ((ServiceSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new ServiceSettings())));
