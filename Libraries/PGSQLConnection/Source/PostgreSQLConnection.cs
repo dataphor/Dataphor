167cd4aab6759be7e959a11146e6e23c8c220356
Jonathan Percival <jonathan.i.percival@gmail.com>
2018-12-12 10:06:00 -0700

    Postgres Transaction Fixes

--- Libraries/PGSQLConnection/Source/PostgreSQLConnection.cs
+++ Libraries/PGSQLConnection/Source/PostgreSQLConnection.cs
@@ -14,19 +14,19 @@ namespace Alphora.Dataphor.DAE.Connection.PGSQL
 	public class PostgreSQLConnection : DotNetConnection
 	{
         public PostgreSQLConnection(string connection) : base(connection) { }
-		
-		protected override IDbConnection CreateDbConnection(string connectionString)
-		{
-			try
-			{
+
+        protected override IDbConnection CreateDbConnection(string connectionString)
+        {
+            try
+            {
                 return new NpgsqlConnection(connectionString);
-			}
-			catch (Exception exception)
-			{
+            }
+            catch (Exception exception)
+            {
                 WrapException(exception, string.Format("connect ({0})", connectionString), true);
-				throw;
-			}
-		}
+                throw;
+            }
+        }
 		
 		protected override SQLCommand InternalCreateCommand()
 		{
@@ -46,11 +46,77 @@ namespace Alphora.Dataphor.DAE.Connection.PGSQL
 			_transaction = _connection.BeginTransaction(_isolationLevel);
 		}
 
-		protected override Exception InternalWrapException(Exception exception, string statement)
+        protected override Exception InternalWrapException(Exception exception, string statement)
 		{
-			// Wrap all exceptions coming back with a simple Exception so that it crosses the boundary.
-			return new ConnectionException(ConnectionException.Codes.SQLException, ErrorSeverity.Application, new Exception(exception.Message), statement);
+            ErrorSeverity severity = GetExceptionSeverity(exception);
+            if (severity == ErrorSeverity.User)
+                return new DataphorException(ErrorSeverity.User, DataphorException.ApplicationError, exception.Message);
+
+            return base.InternalWrapException(exception, statement);
 		}
-	}
+
+        protected override bool IsTransactionFailure(Exception exception)
+        {
+            NpgsqlException localException = exception as NpgsqlException;
+            if (localException != null)
+            {
+                foreach (NpgsqlError error in localException.Errors)
+                {
+                    if (IsTransactionError(error.Code))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsTransactionError(string errorCode)
+        {
+            return !IsUserCorrectableError(errorCode);
+        }
+
+        private bool IsUserCorrectableError(string errorCode)
+        {
+            string classCode = errorCode.Substring(0, 2);
+
+            switch (classCode)
+            {
+                // Warnings
+                case "01":
+                case "02":
+                // Statement not yet complete
+                case "03":
+                // Data exception
+                case "22":
+                // Integrity constraint
+                case "23": return true;
+                // Transaction state
+                case "25": return false;
+                default: return false;
+            }
+        }
+
+        private ErrorSeverity GetExceptionSeverity(Exception exception)
+        {
+            // If the error code indicates an integrity constraint violation or other user-correctable message, severity is user, otherwise, severity is application
+            NpgsqlException localException = exception as NpgsqlException;
+            if (localException != null)
+            {
+                foreach (NpgsqlError error in localException.Errors)
+                {
+                    if (!IsUserCorrectableError(error.Code))
+                    {
+                        return ErrorSeverity.Application;
+                    }
+                }
+
+                return ErrorSeverity.User;
+            }
+
+            return ErrorSeverity.Application;
+        }
+    }
 }
 
